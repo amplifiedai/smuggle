ce1d9b2229d15ecbfe2a33e3df36ee6e0086bd52
Garth Kidd <garth@amplified.ai>
2021-03-01 14:58:23 +1100

    Remove unused :decorator runtime dependency

--- mix.exs
+++ mix.exs
@@ -30,7 +30,6 @@ defmodule Smuggle.MixProject do
   defp deps do
     [
       {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
-      {:decorator, "~> 1.3.2"},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.13.0", only: :test, runtime: false},
